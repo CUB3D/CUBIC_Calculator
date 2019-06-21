3573e8511386e1a808081bc913b173f1b8ec7251
CUB3D <callumthom11@gmail.com>
2019-06-21 17:35:23 +0100

    First Commit

---
+++ app/src/main/java/pw/cub3d/calculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package pw.cub3d.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_grid.isStretchAllColumns = true
+    }
+}
